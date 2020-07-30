e74ed61bdba931fa14a4a7b37e667fa5b3752f61
tomoyakun1221 <tomoyakun.1221@gmail.com>
2020-07-31 02:29:26 +0900

    devise_user_マイグレーション完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # resources :users
+  devise_for :users
+  resources :users
 
-  # root 'users#show'
+  root 'users#show'
 
-  # get 'user/show'
-  # get '/users' => redirect("users/sign_in")
-  # get '/users/password' => redirect("users/password/new")
+  get 'user/show'
+  get '/users' => redirect("users/sign_in")
+  get '/users/password' => redirect("users/password/new")
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
