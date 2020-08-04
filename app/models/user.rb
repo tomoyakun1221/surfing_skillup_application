aa94ec861897331237af4ee710b04298b2d48a36
tomoyakun1221 <tomoyakun.1221@gmail.com>
2020-08-05 01:17:38 +0900

    管理者専用ページ_形完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,14 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :surf_spots, dependent: :destroy
   
-  attr_accessor :remember_token
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_TEL_REGEX = /\A\d{10,11}\z/
 
-  # validates :hourly_wage, presence: true
   validates :tel, presence: true, format: { with: VALID_TEL_REGEX }, length: { maximum: 11 }
   validates :name, presence: true, length: { maximum: 50 }
   validates :birthday, presence: true
   validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
-  def namename
-    name + " " + name_call
-  end
 end
